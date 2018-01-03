b15b1ae24f429b018bd821db8c4109e157a834e5
ovu <omar.vera@gmail.com>
2018-01-03 11:11:57 +0100

    Removed duplicated file

--- .config/fish/cdf.fish
+++
@@ -1,5 +0,0 @@
-function cdf --description 'select one directory using fzf'
-  set -l foldersToExclude '\.git\|\.vscode\|node_modules\|bower\|logs\|generated\|dist\|build\|\.DS_Store'
-  find . -type d | grep -v $foldersToExclude | fzf | read -l selectedDir
-  cd $selectedDir
-end
