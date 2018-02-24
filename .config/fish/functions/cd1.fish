c744e5c7029ec4fc327218c3c537711f0040c3df
Diego Vera <omar.vera@gmail.com>
2018-02-24 12:19:36 +0100

    Improved performance of fish function cd1

--- .config/fish/functions/cd1.fish
+++ .config/fish/functions/cd1.fish
@@ -1,5 +1,5 @@
 function cd1 --description 'select one directory searching one level using fzf'
-  find . -type d -d 1 | fzf | read -l selectedDir
+  ls -d */ | fzf | read -l selectedDir
   if [ $selectedDir ]
    cd $selectedDir
   end 
