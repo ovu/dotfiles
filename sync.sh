a935d0a269ed7d716f158366aeda80e77bdbcea7
Diego Vera <omar.vera@gmail.com>
2018-02-24 12:24:25 +0100

    Do not sync .gitconfig

--- sync.sh
+++ sync.sh
@@ -3,7 +3,7 @@ cd "$(dirname "$0")"
 git pull
 
 copyFiles() {
-  rsync --exclude ".git/" --exclude ".DS_Store" --exclude "sync.sh" --exclude "README.md" --exclude "iTerm2" --exclude "terminal" --exclude "install-deps.sh" --exclude "readme.md" --exclude "createlinks.sh" --exclude "installers/"  -av . ~
+  rsync --exclude ".git/" --exclude ".DS_Store" --exclude "sync.sh" --exclude "README.md" --exclude "iTerm2" --exclude "terminal" --exclude "install-deps.sh" --exclude "readme.md" --exclude "createlinks.sh" --exclude "installers/" --exclude ".gitconfig"  -av . ~
 }
 
 copyFiles
